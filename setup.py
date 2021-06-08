c8c7b8661a04114ff5c83cff5bff450441aec9df
Aleksandr Stepanov <sa@eri.su>
2021-06-08 21:58:47 +0300

    close sources

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-
-from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 from glob import glob
 
 import os, sys
-from distutils.core import setup
+
+
 #from distutils.command.build_ext import build_ext
 
 from datetime import datetime
@@ -15,10 +15,6 @@ setup(
     name="URadius",
     version="1." + datetime.now().strftime('%Y.%m%d'),
 
-    cmdclass={
-        'build_ext': build_ext
-    },
-
     install_requires=[
           'sslkeylog', 'uvloop', 'pycrypto', 'aenum', 'asyncache'
     ],
@@ -26,7 +22,7 @@ setup(
     package_data={'radius': ['dictionary/dict*', 'certs/*.pem']},
 
     ext_modules = cythonize(
-         ['radius/packet.py'] + glob('radius/mschap/*.py'),
+         glob('radius/*.pyx') + glob('radius/mschap/*.py'),
          compiler_directives={'language_level' : "3"}
     ),
     python_requires='>=3.6',
